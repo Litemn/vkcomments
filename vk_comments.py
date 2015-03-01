9186f3425e4e575afda60969c1ad06c12e4c4227
sultee <sultan@MacBook-Air-Sultan.local>
2015-03-02 00:36:30 +0200

    Without comments

--- vk_comments.py
+++ vk_comments.py
@@ -13,10 +13,9 @@ userid = raw_input("Enter userid: ")
 
 user = vk.users.get(user_ids=userid)
 
-#sultan = vk.users.get(user_ids='sultee')
+
 posts = vk.wall.get(owner_id=group_id)
 
-#comms = vk.wall.getComments(owner_id='-76982440', post_id = '83033', offset=75, count=100)
 
 comms = []
 for i in posts[1:]:
@@ -37,8 +36,3 @@ for k in comms:
     comm_counter = int(math.ceil(k[1]/100))
     for l in range(comm_counter+1):
         checkcomms(k[0], l*100)
-#    stena = vk.wall.getComments(owner_id = group_id, post_id = i[0], count=100)
-#    for j in stena[1:]:
-#            if j['from_id']== user[0]['uid']:
-#                print j['text']
-#    time.sleep(0.4)
